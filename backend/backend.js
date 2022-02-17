f4b0a70d172d5131f9fdfadc26bc882aa4cbad1b
reljadev <milan54321@hotmail.com>
2022-02-17 11:26:13 +0100

    set up node server

--- backend/backend.js
+++ backend/backend.js
@@ -1,18 +1,69 @@
 const http = require('http')
 const fs = require('fs')
-const port = 3200
+var path = require('path');
 
-const server = http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile('/home/relja/src/bughouse.js/index.html', function(error, data) {
-        if(error) {
-            response.writeHead(404)
-            response.write('Error: File Not Found')
-        } else {
-            response.write(data)
+const port = 3000
+
+// serve static files
+// TODO: if you don't use, remove module
+// with express library 
+//const express = require('express')
+//const app = express()
+//const server = http.createServer(app)
+//app.use(express.static('../frontend'))
+
+// const server = http.createServer(function(request, response) {
+    
+//     fs.readFile('../frontend/index.html', function(error, data) {
+//         if(error) {
+//             response.writeHead(404)
+//             response.write('Error: File Not Found')
+//         } else {
+//             response.writeHead(200, {'Content-Type': 'text/html'})
+//             response.write(data)
+//         }
+//         response.end()
+//     })
+// })
+
+// without express library
+const server = http.createServer(function (request, response) {
+    console.log('requesting ' + request.url);
+
+    const dirpath = '../frontend';
+    if(request.url === '/') {
+        var filePath = dirpath + '/index.html'
+    } else {
+        var filePath = dirpath + request.url;
+    }
+
+    var extname = path.extname(filePath);
+    const ext_to_type = {'.html': 'text/html', '.js': 'text/javascript',
+                         '.css': 'text/css', '.json': 'application/json',
+                         '.png': 'image/png', '.jpg': 'image/jpg',
+                         '.wav': 'audio/wav'}
+    var contentType = ext_to_type[extname]
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
         }
-        response.end()
-    })
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
 })
 
 server.listen(port, function(error) {
