5c665fffd19d408777487c8435c04ba1ad3def56
reljadev <milan54321@hotmail.com>
2022-02-19 21:57:02 +0100

    restructured folder architecture

--- backend/backend.js
+++ backend/backend.js
@@ -32,10 +32,14 @@ const server = http.createServer(function (request, response) {
     console.log('requesting ' + request.url);
 
     const dirpath = '../frontend';
+    //TOOD: this is an ugly hack
+    let file_name = request.url.substring(0, request.url.indexOf('.'))
     if(request.url === '/') {
-        var filePath = dirpath + '/login/login.html'
+        var filePath = dirpath + '/game/game.html'
+    } else if(file_name === '/game' || file_name === '/landing_page'){
+        var filePath = dirpath + file_name + request.url
     } else {
-        var filePath = dirpath + request.url;
+        var filePath = dirpath + request.url
     }
 
     var extname = path.extname(filePath);
@@ -47,7 +51,7 @@ const server = http.createServer(function (request, response) {
 
     fs.readFile(filePath, function(error, content) {
         if (error) {
-            if(error.code == 'ENOENT'){
+            if(error.code == 'ENOENT') {
                 fs.readFile('./404.html', function(error, content) {
                     response.writeHead(200, { 'Content-Type': contentType });
                     response.end(content, 'utf-8');
@@ -75,10 +79,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('A user has disconnected.');
     })
-
-    socket.on('buttonClicked', () => {
-        console.log('Button clicked arrived to server')
-    });
+    
 });
 
 server.listen(port, function(error) {
